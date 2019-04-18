a0e3d19aa23736b432c045f0601a54a9e2bdda20
duckhee <shldhee@yahoo.co.kr>
2019-04-18 18:00:03 +0900

    working ajax progress

--- app.js
+++ app.js
@@ -16,6 +16,36 @@ app.get('/', function(req, res) {
   res.sendFile(__dirname + "/public/main.html")
 });
 
+app.post("/ajax_send_form", function (req, res) {
+  const yourName = req.body.yourName;
+  const partnerName = req.body.partnerName;
+
+  unirest.get("https://love-calculator.p.rapidapi.com/getPercentage?fname="+yourName+"&sname="+partnerName)
+  .header("X-RapidAPI-Host", "love-calculator.p.rapidapi.com")
+  .header("X-RapidAPI-Key", "832f7e1b70msh3824ed4b1a4d2c1p1a506bjsn25e5cbaccf8c")
+  .end(function (result) {
+    // console.log(result);
+    let resultData = {
+       'yourName' : result.body.fname,
+       'partnerName' : result.body.sname,
+       'percentage' : result.body.percentage,
+       'resultNum' : result.body.result  
+    }
+    res.json(resultData);
+    // res.send("post reqspone");
+    // res.render('love.ejs', {
+    //   'yourName' : yourName,
+    //   'partnerName' : partnerName,
+    //   'percentage' : result.body.percentage,
+    //   'resultNum' : result.body.result
+    // })
+  });
+
+
+  // res.json(responseData);
+})
+
+/*
 app.post('/love_post', function(req, res) {
   // get일 경우 : req.param('email') 이런식으로 받을 수 있다고 함
   // console.log(req.body); // post
@@ -37,8 +67,5 @@ app.post('/love_post', function(req, res) {
     })
   });
 })
+*/
 
-app.post("/ajax_send_form", function (req, res) {
-  var responseData = req.body;
-  res.json(responseData);
-})
