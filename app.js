1dad5a9b1ddf1a1865e77e9b0ac7c5ff3787949f
duckhee <shldhee@yahoo.co.kr>
2019-04-18 13:56:04 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const unirest = require('unirest');
+
+app.listen(3000, function () {
+  console.log("start !!! express server on port 3000~");
+});
+
+app.use(express.static('public'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.set('vie engine', 'ejs');
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + "/public/main.html")
+});
+
+app.post('/love_post', function(req, res) {
+  // get일 경우 : req.param('email') 이런식으로 받을 수 있다고 함
+  // console.log(req.body); // post
+
+  const yourName = req.body.yourName;
+  const partnerName = req.body.partnerName;
+
+  unirest.get("https://love-calculator.p.rapidapi.com/getPercentage?fname="+yourName+"&sname="+partnerName)
+  .header("X-RapidAPI-Host", "love-calculator.p.rapidapi.com")
+  .header("X-RapidAPI-Key", "832f7e1b70msh3824ed4b1a4d2c1p1a506bjsn25e5cbaccf8c")
+  .end(function (result) {
+    // console.log(result.body);
+    // res.send("post reqspone");
+    res.render('love.ejs', {
+      'yourName' : yourName,
+      'partnerName' : partnerName,
+      'percentage' : result.body.percentage,
+      'resultNum' : result.body.result
+    })
+  });
+})
+
+// api
